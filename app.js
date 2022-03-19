f9b84ae7f1f8f6d2dfbd27c7b92fb49dc705b843
unknown <vasanth>
2022-03-19 12:07:39 +0530

    level-1 Password and email only

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { log } = require('async');
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+    email : String,
+    password : String
+}
+
+const User = new mongoose.model("User", userSchema)
+
+app.get("/", (req,res) => {
+    res.render("home")
+})
+
+app.get("/login", (req,res) => {
+    res.render("login")
+})
+
+app.post("/login", (req,res)=>{
+    User.findOne({email : req.body.username}, (err, founded)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(founded.password == req.body.password){
+                res.render("secrets")
+            }
+        }
+    })
+})
+
+app.get("/register", (req,res) => {
+    res.render("register")
+})
+
+app.post("/register", (req,res)=>{
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    })
+
+    newUser.save((err) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets")
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server running on Port 3000");
+})
